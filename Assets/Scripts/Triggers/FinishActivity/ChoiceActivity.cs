46d3f7752c838e18f1198d00a93a41a48e11abe7
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-06-17 17:52:31 +0300

    visual fixes

--- Assets/Scripts/Triggers/FinishActivity/ChoiceActivity.cs
+++ Assets/Scripts/Triggers/FinishActivity/ChoiceActivity.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class ChoiceActivity : FinishActivity
 {
     [SerializeField] private ChoiceCanvas _choiceCanvas;
+    [SerializeField] private GameObject _clueCanvas;
 
     public override void Prepare(Player player, Suspect suspect)
     {
         _choiceCanvas.Init();
+        _clueCanvas.SetActive(false);
     }
 }
