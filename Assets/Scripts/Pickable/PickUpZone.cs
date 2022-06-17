91298ebfb62d0160a5bfd0c8a9e3240f243c76e2
PlotvaIzLodzya <86444967+PlotvaIzLodzya@users.noreply.github.com>
2022-06-17 14:55:20 +0300

    boquet

--- Assets/Scripts/Pickable/PickUpZone.cs
+++ Assets/Scripts/Pickable/PickUpZone.cs
@@ -7,6 +7,7 @@ public class PickUpZone : MonoBehaviour
 {
     [SerializeField] private Player _player;
 
+    private float _pickUpTime = 1.3f;    
     private Pickable _pickable;
 
     private void OnTriggerEnter(Collider other)
@@ -15,8 +16,9 @@ public class PickUpZone : MonoBehaviour
         {
             _pickable = pickable;
             _player.handAnimator.TriggerGrab();
-            _player.playerMover.StopMoving(0.75f);
+            _player.playerMover.StopMoving(_pickUpTime);
             _player.cameraHandler.SitDown();
+            StartCoroutine(Delaying());
         }
     }
 
@@ -28,5 +30,14 @@ public class PickUpZone : MonoBehaviour
     public void OnPickUp(Pickable pickable = null)
     {
         _pickable.PickUp();
+    }
+
+    private IEnumerator Delaying()
+    {
+        _player.loupe.Disable();
+
+        yield return new WaitForSeconds(_pickUpTime-0.3f);
+
+        _player.loupe.Enable();
     }
 }
